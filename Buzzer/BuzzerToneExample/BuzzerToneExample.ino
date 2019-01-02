3817ca0c95c5f375ac3ccb48166341da961e182b
corbosiny <cohulse@asu.edu>
2019-01-02 16:21:18 -0700

    Added comments to examples

--- Buzzer/BuzzerToneExample/BuzzerToneExample.ino
+++ Buzzer/BuzzerToneExample/BuzzerToneExample.ino
@@ -12,10 +12,11 @@ void setup()
 int currentTone = 500;
 int incrementor = 5;
 
+//Just sweep making notes between the frequencies 0 and 1500 Hz
 void loop() 
 {
-  tone(BUZZER_PIN, currentTone);
+  tone(BUZZER_PIN, currentTone); // creates a tone at the frequency specified by currentTone in Hz, doesn't shut off until the pin is turned low
   currentTone += incrementor;
-  if(currentTone == MIN_TONE || currentTone == MAX_TONE) {incrementor *= -1;}
-  delay(10);
+  if(currentTone == MIN_TONE || currentTone == MAX_TONE) {incrementor *= -1;} // If we hit one of the ends of our bounds we start incrementing the other way
+  delay(10); //will take about two seconds to sweep the full range of notes
 }
