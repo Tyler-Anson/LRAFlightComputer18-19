24a676d821cb651e96a3e003e8a9de3243f25d30
corbosiny <cohulse@asu.edu>
2019-01-15 16:12:54 -0600

    Added visualizer code for the IMU, also has extensions for PID visualization

---
+++ IMU/LSM9DS1/visualizerDebugger/visualizerDebugger.ino
@@ -0,0 +1,120 @@
+// The SFE_LSM9DS1 library requires both Wire and SPI be
+// included BEFORE including the 9DS1 library.
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+//////////////////////////
+// LSM9DS1 Library Init //
+//////////////////////////
+// Use the LSM9DS1 class to create an object. [imu] can be
+// named anything, we'll refer to that throught the sketch.
+LSM9DS1 imu;
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+////////////////////////////
+// Sketch Output Settings //
+////////////////////////////
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+static unsigned long lastPrint = 0; // Keep track of print time
+
+// Earth's magnetic field varies by location. Add or subtract 
+// a declination to get a more accurate heading. Calculate 
+// your's here:
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
+float roll, pitch, heading; 
+
+void setup() 
+{
+  
+  Serial.begin(9600);
+  
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  if (!imu.begin())
+  {
+    Serial.println("Failed to initialize IMU");
+    while (1)
+      ;
+  }
+}
+
+void loop()
+{
+  updateIMUreadings();
+  calculateIMUstate();
+  updateVisualizer();
+}
+
+void updateIMUreadings()
+{
+  // Update the sensor values whenever new data is available
+  if ( imu.gyroAvailable() )
+  {
+    // To read from the gyroscope,  first call the
+    // readGyro() function. When it exits, it'll update the
+    // gx, gy, and gz variables with the most current data.
+    imu.readGyro();
+  }
+  if ( imu.accelAvailable() )
+  {
+    // To read from the accelerometer, first call the
+    // readAccel() function. When it exits, it'll update the
+    // ax, ay, and az variables with the most current data.
+    imu.readAccel();
+  }
+  if ( imu.magAvailable() )
+  {
+    // To read from the magnetometer, first call the
+    // readMag() function. When it exits, it'll update the
+    // mx, my, and mz variables with the most current data.
+    imu.readMag();
+  } 
+}
+
+void calculateIMUstate()
+{
+  roll = atan2(imu.ay, imu.az);
+  pitch = atan2(-imu.ax, sqrt(imu.ay * imu.ay + imu.az * imu.az));
+  
+  if (imu.my == 0)
+    heading = (imu.mx < 0) ? PI : 0;
+  else
+    heading = atan2(imu.mx, imu.my);
+    
+  heading -= DECLINATION * PI / 180;
+  
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+}
+
+void updateVisualizer()
+{
+  sendDataPacket('p', pitch);
+  sendDataPacket('r', roll);
+  sendDataPacket('y', heading);
+}
+
+void sendDataPacket(char param, float value)
+{
+  Serial.print(param);
+  Serial.print(':');
+  Serial.println(value);
+}
